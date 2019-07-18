5ebc335db8d52eb1d0111f50953e995a7a745df0
Mak Abdennabi <hello@makabde.me>
2019-07-18 18:53:38 +0200

    Update Dotfiles directory

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -8,7 +8,7 @@ declare -r DOTFILES_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_REPOSIT
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-declare dotfilesDirectory="$HOME/Projects/Development/dotfiles"
+declare dotfilesDirectory="$HOME/Dotfiles"
 declare skipQuestions=false
 
 # ----------------------------------------------------------------------
