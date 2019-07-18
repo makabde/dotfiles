b32d6e86ad42a1e6616eab422eeb90bdce578247
Mak Abdennabi <hello@makabde.me>
2019-07-18 19:51:06 +0200

    Make sure ASDF dependencies are installed
    https://asdf-vm.com/#/core-manage-asdf-vm?id=plugin-dependencies

--- src/os/install/macos/cli_tools.sh
+++ src/os/install/macos/cli_tools.sh
@@ -7,3 +7,15 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 print_in_purple "\n   CLI Tools\n\n"
+
+brew_install "coreutils" "coreutils"
+brew_install "automake" "automake"
+brew_install "autoconf" "autoconf"
+brew_install "openssl" "openssl"
+brew_install "libyaml" "libyaml" # Must come after openssl
+brew_install "readline" "readline"
+brew_install "libxslt" "libxslt"
+brew_install "libtool" "libtool"
+brew_install "unixodbc" "unixodbc"
+brew_install "unzip" "unzip"
+brew_install "curl" "curl"
