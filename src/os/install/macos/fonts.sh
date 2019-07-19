807acccda93c8d1f3569acb6f084c5a92f9cbe81
Mak Abdennabi <hello@makabde.me>
2019-07-19 13:44:14 +0200

    🎗Create installer for fonts

--- src/os/install/macos/fonts.sh
+++ src/os/install/macos/fonts.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../../utils.sh" \
+    && . "./utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+print_in_purple "\n   Fonts\n\n"
+
+brew_install "Fira-Code" "font-fira-code" "homebrew/cask-fonts" "cask"
+brew_install "Hack" "font-hack" "homebrew/cask-fonts" "cask"
